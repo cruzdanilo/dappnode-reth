f4bcc33cce48f0f3228ac6c19aa2c032907ce3b5
danilo neves cruz <cruzdanilo@gmail.com>
2024-09-26 23:03:20 -0300

    ✨ entrypoint: run reth node

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,3 +30,14 @@ esac
 # print the jwt to the dappmanager
 JWT=$(cat $JWT_PATH)
 curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
+
+# shellcheck disable=SC2086
+exec reth node \
+  --metrics 0.0.0.0:9001 \
+  --log.file.directory /root/logs \
+  --authrpc.jwtsecret ${JWT_PATH} \
+  --authrpc.addr 0.0.0.0 --authrpc.port 8551 \
+  --http --http.addr 0.0.0.0 --http.port 8545 \
+  --ws --ws.addr 0.0.0.0 --ws.port 8546 \
+  --http.api eth,net,web3,txpool \
+  $EXTRA_OPTS
